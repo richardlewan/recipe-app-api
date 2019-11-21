5dfde632a3e4b117c780baa647fcf6757c6b43a4
Dick Lewan <rlewan@Dicks-iMac.local>
2019-11-21 12:19:08 -0700

    Build out core managment for user and admin; initial api for recipe tags.

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,54 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from core import models
+
+
+def sample_user(email='test@richardlewan.com', password='testpass'):
+    """ Create a sample user """
+    return get_user_model().objects.create_user(email, password)
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = "test@richardlewan.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is not case sensitive"""
+        email = 'test@RICHARDLEWAN.com'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@richardlewan.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Bill'
+        )
+
+        self.assertEqual(str(tag), tag.name)
